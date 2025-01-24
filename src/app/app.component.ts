83f31f1a13a8e64d76bc19f370a84dc591ffd196
Ryan Blajda <ryan.j.blajda@gmail.com>
2025-01-24 11:17:13 -0500

    added functionality to get user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, OnInit } from '@angular/core';
 //import data from '../assets/scope_questions.json';
-import { QuestionList } from './components/interfaces/interfaces';
+import { Question, QuestionList } from './components/interfaces/interfaces';
 import { signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -12,12 +12,26 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppComponent implements OnInit {
   title = 'Functional Scope Generator';
+
   questions!:QuestionList;
-  projectNumber = signal<string>('PR-XXXXX');
-  projectDescription = signal<string>('a small huddle room');
-  projectClientName = signal<string>('company or school name');
+  
+  projectNumberPlaceholder = signal<string>('PR-XXXXX');
+  projectNumber!: string;
+  projectDescriptionPlaceholder = signal<string>('a small huddle room');
+  projectDescription!: string;
+  projectClientNamePlaceholder = signal<string>('company or school name');
+  projectClientName!: string;
+
+  customDetails:string | string = '';
+
   success = signal<boolean>(false);
   loading = signal<boolean>(true);
+
+  formCompleted = computed<boolean>(() => this.checkQuestionsStatus());
+  
+  downloadReady = signal<boolean>(false);
+  downloadName = signal<string>('scope.txt');
+  downloadURL = signal<string>('/scope.txt');
 
   private httpClient: HttpClient;
 
@@ -26,6 +40,11 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    //set defaults for generation to placeholders
+    this.projectClientName = this.projectClientNamePlaceholder();
+    this.projectDescription = this.projectDescriptionPlaceholder();
+    this.projectNumber = this.projectNumberPlaceholder();
+    //attempt to load the question json so we can generate the body of the page
     try {
       this.httpClient.get('assets/scope_questions.json', {responseType: 'text'}).subscribe(data => {
         //console.log(data);
@@ -50,7 +69,98 @@ export class AppComponent implements OnInit {
     //}, 3000);
   }
 
-  generateScope():void {
-    console.log(this.questions);
+  checkQuestionsRecursive(questions: Question[]):boolean {
+    let status = false;
+    questions.forEach(question => {
+      question.answers.forEach(answer => {
+        if (answer.selected) {
+          console.log('answer selected')
+          status = true;
+          return;
+        }
+      });
+    });
+    console.log(`return ${status}`)
+    return status;
+  }
+
+  checkQuestionsStatus():boolean {
+    let status = false;
+    this.questions.sections.forEach(section => {
+      status = this.checkQuestionsRecursive(section.questions);
+    });
+    return status;
+  }
+
+  parseQuestionRecursive(questions:Question[]): string {
+    let questionTextData:string = "";
+    //loop through all the questions
+    questions.forEach(question => {
+      //loop through all the answers for the question we are on
+      question.answers.forEach(answer => {
+        //only return an answer if its selected
+        if (answer.selected) {
+          //if the scope field is empty, return the question prompt instead.
+          if (answer.scope.length > 0) { questionTextData += answer.scope + " "; }
+          else { 
+            if (answer.prompt.toLowerCase() == "no" || answer.prompt.toLowerCase() == "yes") { /* dont return anything */ }
+            else { questionTextData += answer.prompt + " ";   }
+          }
+          //if the answer has children questions, parse recursively
+          if (answer.questions != undefined) {
+            if (answer.questions.length > 0) { questionTextData += this.parseQuestionRecursive(answer.questions); + "\r\n"; }
+          }
+        }
+      });
+    });
+    //return the string
+    return questionTextData;
+  }
+
+  parseResponses(list:QuestionList): string {
+    let fileTextData:string = "";
+    //loop through each section
+    list.sections.forEach(section => {
+      //grab the section name
+      fileTextData += section.name + "\r\n\t";
+      //parse all the questions for this section recursively
+      fileTextData += this.parseQuestionRecursive(section.questions) + "\r\n";
+    });
+    //return the completed scope
+    return fileTextData;
+  }
+
+  generateScope():string {
+    //create the initial string, and generate the header details
+    let scope:string = `CCS Presentation Systems\r\n\r\n${this.projectClientName}\r\n${this.projectNumber} // ${this.projectDescription}\r\n\r\nFunctional Programming Scope\r\n\r\n`;
+    //generate the main body of the scope
+    scope += this.parseResponses(this.questions);
+    //if the custom details section has a length, add it to the bottom of the scope
+    if (this.customDetails.length > 0 ) { scope += `Custom Scope Requirements\r\n\r\n\t${this.customDetails}`; }
+    return scope;
+  }
+
+  onInput(sender:string, event:Event): void {
+    if (sender == 'project-num') { this.projectNumber = (event.target as HTMLInputElement).value; }
+    else if (sender == 'project-client') { this.projectClientName = (event.target as HTMLInputElement).value; }
+    else if (sender == 'project-description') { this.projectDescription = (event.target as HTMLInputElement).value; }
+    else if (sender == 'custom-details') { this.customDetails = (event.target as HTMLTextAreaElement).value; }
+  }
+
+  onGenerateScopeClicked():void {
+    //console.log(this.questions);
+    //parse the responses from the end user
+    let scope = this.generateScope();
+    //print it for me
+    //console.log(scope);
+    //generate a new file to download
+    let file = new Blob([scope], {type: '.txt'});
+    this.downloadURL.set(URL.createObjectURL(file));
+    this.downloadName.set(`${this.projectNumber}_${this.projectClientName}_functional_scope.txt`);
+    this.downloadReady.set(true);
+  }
+
+  downloaded(): void {
+    this.downloadReady.set(false);
   }
 }
