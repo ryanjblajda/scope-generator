3529911c15bb34b126b0053256da7ec581beacc7
Ryan Blajda <ryan.j.blajda@gmail.com>
2025-01-27 21:41:49 -0500

    adjust formatting to display properly

--- src/app/components/fullscreen-message/fullscreen-message.component.ts
+++ src/app/components/fullscreen-message/fullscreen-message.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-fullscreen-message',
   templateUrl: './fullscreen-message.component.html',
   styleUrls: ['./fullscreen-message.component.scss']
 })
-export class FullscreenMessageComponent {
+export class FullscreenMessageComponent implements OnChanges {
   @Input() header:string = 'header';
   @Input() message:string = 'message';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes != null) {
+      if (changes['header'].currentValue != this.header) { this.header == changes['header'].currentValue }
+      else if (changes['message'].currentValue != this.message) { this.message == changes['message'].currentValue }
+    }
+  }
 }
