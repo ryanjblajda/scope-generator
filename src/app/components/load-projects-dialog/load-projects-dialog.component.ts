4a1fe0aad327bcb87bc192e1e6415e64b88c7c03
Ryan Blajda <ryan.j.blajda@gmail.com>
2025-01-28 09:10:55 -0500

    created component to hold existing projects for loading

---
+++ src/app/components/load-projects-dialog/load-projects-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-load-projects-dialog',
+  templateUrl: './load-projects-dialog.component.html',
+  styleUrls: ['./load-projects-dialog.component.scss']
+})
+export class LoadProjectsDialogComponent {
+
+}
